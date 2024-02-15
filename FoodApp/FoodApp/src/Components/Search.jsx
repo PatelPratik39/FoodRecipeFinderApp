bff173aef8ccc0f342ffaea1ca85057ab242ada3
Pratik Patel <pratspatel17791@gmail.com>
2024-02-15 13:36:21 -0600

    sc

--- FoodApp/FoodApp/src/Components/Search.jsx
+++ FoodApp/FoodApp/src/Components/Search.jsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
-import styles from './search.module.css'
+import styles from "./search.module.css";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "8765847114e944939f129a3410b3d278";
 
-const Search = ({foodData, setFoodData}) => {
+const Search = ({ foodData, setFoodData }) => {
   const [query, setQuery] = useState("Pizza");
 
   useEffect(() => {
     async function fetchFood() {
       const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await response.json();
-    //   console.log(data.results);
-      setFoodData(data.results);  
+      //   console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
 
   return (
     <div className={styles.searchContainer}>
+      <h2>Search your Food Item 🔎 : </h2>
       <input
         type="text"
         className={styles.input}
